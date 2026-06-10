bfbdbc19381450d4d6bbe36662a934fe5d6ea702
Roman <kravtsov1286roman@gmail.com>
2026-06-10 11:05:22 +0000

    add Tailwind & PrimeVue setup with login page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+import primevue   from './primevue.config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  
+  modules: ['@primevue/nuxt-module'],
+ // @ts-ignore:
+  primevue
 })
