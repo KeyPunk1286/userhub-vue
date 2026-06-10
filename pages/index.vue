6563753718cc09886078bde8129c50222bd5847c
Roman <kravtsov1286roman@gmail.com>
2026-06-10 11:12:35 +0000

    feat: add login page and update dependencies

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col gap-4 items-center justify-center h-screen">
+        <h1 class="text-3xl font-bold">Welcome to UserHub</h1>
+        <LoginForm />
+    </div>
+</template>
+
+<script setup>
+import LoginForm from '@/components/FormComponents/LoginForm.vue';
+</script>
---
+++ primevue.config.ts
@@ -0,0 +1,5 @@
+export default {
+  ripple: true,                // ефект хвилі на кнопках
+  inputStyle: 'outlined',      // стиль інпутів: 'outlined' або 'filled'
+  theme: 'aura-light-blue'     // тема з пакету @primeuix/themes
+}
